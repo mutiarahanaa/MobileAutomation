22a374bdc5df25f3ae500ca4bf808850df635310
tiara <mutiara.fatwa@mamiteam.com>
2020-07-27 21:52:08 +0700

    finish setup

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -1,3 +1,19 @@
-public class CucumberRunner {
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {
+                "classpath:features",
+        },
+        stepNotifications = true,
+        plugin = {
+                "pretty",
+                "rerun:rerun/failed_scenarios.txt",
+                "json:target/cucumber-report.json",
+        })
+ public class CucumberRunner {
 
 }
--- src/test/java/myapps/PageObject/HomepagePO.java
+++ src/test/java/myapps/PageObject/HomepagePO.java
@@ -1,16 +1,17 @@
 package myapps.PageObject;
-import id.aldochristiaan.salad.module.Espresso;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 
 
-public class HomepagePO extends Espresso {
+
+public class HomepagePO extends Basepage {
     public HomepagePO(AndroidDriver<AndroidElement> androidDriver) {
         super(androidDriver);
     }
 
     public void validateHonepage(){
-        validateElementVisible("ICONSEARCH");
+        validateDisplayed("ICONSEARCH", 5);
 
     }
+
 }
