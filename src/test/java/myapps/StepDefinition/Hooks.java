4f087c6f169864c14cb2d5c54ab832c96888e4a0
tiara <mutiara.fatwa@mamiteam.com>
2021-01-17 03:31:57 +0700

    long way to go

--- src/test/java/myapps/StepDefinition/Hooks.java
+++ src/test/java/myapps/StepDefinition/Hooks.java
@@ -2,18 +2,24 @@ package myapps.StepDefinition;
 
 import id.aldochristiaan.salad.util.LogUtil;
 import io.cucumber.core.api.Scenario;
+import io.cucumber.core.event.Status;
+import io.cucumber.java.Before;
 import io.cucumber.java8.En;
 import myapps.TestInstrument;
 
-import static myapps.CucumberTestRunner.setUp;
+import java.util.HashMap;
 
 
 public class Hooks extends TestInstrument implements En {
 
-        //private  static final HashMap<String>, Boolean> featureMap = HashMap<~>();
-        private static  String lastScenarioStatus = "failed";
+        private static final HashMap<String, Boolean> featureMap = new HashMap<String, Boolean>();
+
+        private static Status lastScenarioStatus = Status.FAILED;
+
+
 
         public Hooks () {
+
                 Before(0, () -> {
                         setUp();
                         Runtime.getRuntime().addShutdownHook(new Thread(this::tearDown));
@@ -40,18 +46,37 @@ public class Hooks extends TestInstrument implements En {
                 });
 
                 After(0, (Scenario scenario) -> {
-                        LogUtil.info("Scenario '"+scenario.getName()+"' with status "+scenario.getStatus().lowerCaseName());
+                        LogUtil.info("Scenario '"+scenario.getName()+"' with status "+scenario.getStatus());
                         LogUtil.info("-----------------End of Scenario-----------------");
-                        setLastScenarioStatus(scenario.getStatus().lowerCaseName());
+                        setLastScenarioStatus(scenario.getStatus());
                 });
 
                 After(1, this::afterScenario);
+        }
+
+        private void resetSession() {
+                removeSession();
+                initiateSession();
+        }
+
+        private void setLastScenarioStatus( Status status){
+                lastScenarioStatus = status;
+        }
+
+        private Status getLastScenarioStatus(){
+                return lastScenarioStatus;
         }
 
         private boolean isFeatureNameNotExist(Scenario scenario) {
 
                 return false;
         }
+
+        @Before
+        public static void beforeStep(Scenario mScenario) {
+                scenario = mScenario;
+        }
+
 
 }
 //        private static Salad salad; // Automation Engine
